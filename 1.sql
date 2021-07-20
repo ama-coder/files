89319c595b64f0093c371fbc770b2c4c7be50a04
ama-coder <67456142+ama-coder@users.noreply.github.com>
2021-07-20 12:26:35 +0430

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -5,3 +5,10 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
 `ur_status` int(11) NOT NULL,  
 PRIMARY KEY (`ur_Id`)  
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+
+INSERT INTO `tbl_user` (`ur_Id`, `ur_username`, `ur_password`, `ur_status`) VALUES  
+(1, 'masoudline@gmail.com', 'مسعود', 1),  
+(2, 'support@roxo.ir', 'انجمن روکسو', 1),  
+(3, 'info@roxo.ir', 'برنامه نویسی روکسو', 1);
