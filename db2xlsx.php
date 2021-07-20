ae7872ef0adebc15a0eb5146fc127c835bdbb39e
ama-coder <67456142+ama-coder@users.noreply.github.com>
2021-07-20 12:57:56 +0430

    Update db2xlsx.php

--- db2xlsx.php
+++ db2xlsx.php
@@ -1,36 +1,36 @@
 <html>  
   
 <head>  
-    <title>User Report List</title>  
-</head>  
+    <title>User Detail Report</title>  
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
   
-<body>
+<body>  
+    <table border="1">  
+        <tr>  
+            <th>Sr NO.</th>  
+            <th width="120">User Name</th>  
+            <th>Password</th>  
+        </tr>  
+        <?php  
+		
+$conn = new mysqli('localhost', 'root', '');   
+mysqli_select_db($conn, 'test');   
+$conn->set_charset("utf8");
   
- <table border="1">  
-    <tr>  
-        <th>Sr NO.</th>  
-        <th width="120">User Name</th>  
-        <th>Password</th>  
-    </tr>  
-</table> 
- </body>  
+$sql = mysqli_query($conn,"SELECT `ur_Id`,`ur_username`,`ur_password` FROM `tbl_user`");  
+  
+  
+while($data = mysqli_fetch_row($sql)){  
+echo '  
+<tr>  
+<td>'.$data[0].'</td>  
+<td>'.$data[1].'</td>  
+<td>'.$data[2].'</td>  
+</tr>  
+';  
+}  
+?>  
+    </table> <a href="/Export.php"> Export To Excel </a>
+</body>  
   
 </html>
-<?php  
-   $conn = new mysqli('localhost', 'root', '');   
-   mysqli_select_db($conn, 'EmployeeDB');   
-   $conn->set_charset("utf8");
-$sql = mysqli_query($conn,"SELECT `ur_Id`,`ur_username`,`ur_password` FROM `tbl_user`");  
-
-while($data = mysqli_fetch_row($sql))  
-{  
-    echo '  
-    <tr>  
-    <td>'.$data[0].'</td>  
-    <td>'.$data[1].'</td>  
-    <td>'.$data[2].'</td>  
-    </tr>  
-    ';  
-}
-
-?>
